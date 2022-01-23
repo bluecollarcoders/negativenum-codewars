a9cccccda41a2b3ec27ef4cce5a547f741142795
Caleb Matteis <matteismusic@gmail.com>
2022-01-22 23:42:57 -0500

    Your commit message

---
+++ narcissisticNumbers.js
@@ -0,0 +1,19 @@
+/*
+A Narcissistic Number is a number of length n in which the sum of its digits to the power of n is equal to the original number. If this seems confusing, refer to the example below.
+
+Ex: 153, where n = 3 (number of digits in 153)
+13 + 53 + 33 = 153
+
+Write a method is_narcissistic(i) (in Haskell: isNarcissistic :: Integer -> Bool) which returns whether or not i is a Narcissistic Number.
+
+*/
+
+const isNarcissistic = (n) => {
+  //your code here
+  var num = 0;
+  var numbers = String(n);
+  for (let i of numbers) {
+    num += Number(i) ** numbers.length;
+  }
+  return num === n;
+};
