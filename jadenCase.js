af8b221d498e6a3278b767991218b081afa90030
Caleb Matteis <matteismusic@gmail.com>
2022-05-13 11:22:31 -0400

    commit

---
+++ jadenCase.js
@@ -0,0 +1,11 @@
+String.prototype.toJadenCase = function () {
+  //   assign 'this' keyword to a variable and split string into an array
+  var result = this.split(" ");
+
+  //   loop through the array changing first character of each item uppercase & adding it to the remaing letters in each item
+  for (let i = 0; i < result.length; i++) {
+    result[i] = result[i].charAt(0).toUpperCase() + result[i].substring(1);
+  }
+  //   return items joined back together in a string
+  return result.join(" ");
+};
