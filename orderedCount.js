2c7f8b28f1eea64158443d51c488978124ce1388
Caleb Matteis <matteismusic@gmail.com>
2022-08-11 23:57:36 -0400

    commit

---
+++ orderedCount.js
@@ -0,0 +1,21 @@
+/*
+
+
+Count the number of occurrences of each character and return it as a list of tuples in order of appearance. For empty output return an empty list.
+
+
+
+
+*/
+
+const orderedCount = (text) => {
+  var strSplit = text.split("");
+  const str = [...new Set(strSplit)];
+
+  var newArray = [];
+
+  for (let i = 0; i <= str.length - 1; i += 1) {
+    newArray.push([str[i], strSplit.filter((s) => s === str[i]).length]);
+  }
+  return newArray;
+};
