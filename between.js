e5724cfc70e03e3d9d588a01f793ee3741c3ad8b
Caleb Matteis <matteismusic@gmail.com>
2022-11-14 20:00:00 -0600

    commit

---
+++ between.js
@@ -0,0 +1,21 @@
+/*
+Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+*/
+
+const between = (a, b) => {
+  // create an empty array
+  // loop through a and b parameters
+  // push the result to new array
+  var holder = [];
+  for (let i = a; i <= b; i++) {
+    holder.push(i);
+  }
+  return holder;
+};
